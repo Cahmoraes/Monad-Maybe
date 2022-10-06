4c8a50d59dd0e400ec3f4ee1bf7de8346d0be43a
caique.moraes <caique.moraes@compasso.com.br>
2022-10-06 16:16:55 -0300

    update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,14 @@
-declare const deepStrictEqual: (objA: any, objB: any) => boolean;
+declare type CallbackM<T> = (value: T) => any;
+declare type CallbackChainM<T> = (value: T) => Maybe<any>;
+declare class Maybe<T> {
+    private value;
+    constructor(value: T);
+    static of<T>(value: T): Maybe<T>;
+    map(callbackMap: CallbackM<T>): Maybe<T>;
+    chain(callbackChain: CallbackChainM<T>): Maybe<T>;
+    getOrElse<K>(defaultValue: K): T | K;
+    private join;
+    private isEmpty;
+}
 
-export { deepStrictEqual };
+export { Maybe };
