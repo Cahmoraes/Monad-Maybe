db8b42a91be1243a149a1d92b3f1f246235e7d03
caique.moraes <caique.moraes@compasso.com.br>
2022-10-07 20:58:05 -0300

    improve typescript intelliSense

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,11 @@
-declare type CallbackM<T> = (value: T) => any;
-declare type CallbackChainM<T> = (value: T) => Maybe<any>;
+declare type CallbackM<T, U> = (value: T) => U;
+declare type CallbackChainM<T, U> = (value: T) => Maybe<U>;
 declare class Maybe<T> {
     private value;
     constructor(value: T);
     static of<T>(value: T): Maybe<T>;
-    map(callbackMap: CallbackM<T>): Maybe<T>;
-    chain(callbackChain: CallbackChainM<T>): Maybe<T>;
+    map<U>(callbackMap: CallbackM<T, U>): Maybe<U>;
+    chain<U>(callbackChain: CallbackChainM<T, U>): Maybe<U>;
     getOrElse<K>(defaultValue: K): T | K;
     private join;
     private isEmpty;
