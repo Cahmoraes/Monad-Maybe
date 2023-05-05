2a54dc74062060a92854d4fc694681c850c5df1e
caique.moraes <ike.moraes93@gmail.com>
2023-05-05 15:55:10 -0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -12,6 +12,7 @@ declare class Maybe<InitialType> {
     private readonly value;
     constructor(value: InitialType);
     static of<T>(value: T): Maybe<T>;
+    static empty(): Maybe<any>;
     map<TransformedType>(callbackMap: Callback<InitialType, TransformedType>): Maybe<TransformedType>;
     isNothing(): this is null | undefined;
     chain<TransformedType>(callbackChain: CallbackChain<InitialType, TransformedType>): Maybe<TransformedType>;
