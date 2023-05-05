21f31a061ec2301cde5a1b85687151c58155e191
caique.moraes <ike.moraes93@gmail.com>
2023-05-05 10:32:40 -0300

    update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,14 @@
-type CallbackM<T, K> = (value: T) => K;
-type CallbackChainM<T, K> = (value: T) => Maybe<K>;
-declare class Maybe<T> {
-    private value;
-    constructor(value: T);
+type Callback<InitialType, TransformedType> = (value: InitialType) => TransformedType;
+type CallbackChain<InitialType, TransformedType> = (value: InitialType) => Maybe<TransformedType>;
+declare class Maybe<InitialType> {
+    private readonly value;
+    constructor(value: InitialType);
     static of<T>(value: T): Maybe<T>;
-    map<U>(callbackMap: CallbackM<T, U>): Maybe<U>;
-    chain<K>(callbackChain: CallbackChainM<T, K>): Maybe<K>;
-    getOrElse<K>(defaultValue: K): T | K;
-    private join;
+    map<U>(callbackMap: Callback<InitialType, U>): Maybe<U>;
     private isEmpty;
+    chain<TransformedType>(callbackChain: CallbackChain<InitialType, TransformedType>): Maybe<TransformedType>;
+    private join;
+    getOrElse<OptionalType = InitialType>(defaultValue: OptionalType): InitialType | OptionalType;
 }
 
 export { Maybe };
