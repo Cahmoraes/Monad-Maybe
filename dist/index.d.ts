a8a9279e35a0ce5472341d61c9d948f1909c2feb
caique.moraes <ike.moraes93@gmail.com>
2023-05-25 20:21:13 -0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,10 @@
 declare class Nothing implements Maybe<Nothing> {
-    private _value;
-    orDefault<DefaultType>(defaultValue: DefaultType): DefaultType;
-    map<TransformedType>(_: Callback<never, TransformedType>): Maybe<TransformedType>;
     isNothing(): this is Nothing;
+    map<TransformedType>(_: Callback<never, TransformedType>): Maybe<TransformedType>;
     chain<TransformedType>(_: CallbackChain<never, TransformedType>): Maybe<TransformedType>;
+    filter<Type>(_: never): Maybe<Type>;
+    orDefault<DefaultType>(defaultValue: DefaultType): DefaultType;
+    orDefaultLazy<T>(callbackDefaultLazy: CallbackDefaultLazy<T>): T;
     getSafe(): GetSafeReturn<never>;
 }
 
@@ -12,18 +13,23 @@ type CallbackChain<InitialType, TransformedType> = (value: InitialType) => Maybe
 type GetSafeErrorReturn = {
     success: false;
 };
-type GetSafeSuccessReturn<InicialType> = {
+type GetSafeSuccessReturn<InitialType> = {
     success: true;
-    data: InicialType;
+    data: InitialType;
 };
-type GetSafeReturn<InicialType> = GetSafeSuccessReturn<InicialType> | GetSafeErrorReturn;
+type GetSafeReturn<InitialType> = GetSafeSuccessReturn<InitialType> | GetSafeErrorReturn;
+type CallbackDefaultLazy<InitialType> = () => InitialType;
+type CallbackPredicate<InitialType, NextType extends InitialType> = (value: InitialType) => value is NextType;
 interface Maybe<InitialType> {
     map<TransformedType>(callbackMap: Callback<InitialType, TransformedType>): Maybe<TransformedType>;
     isNothing(): this is Nothing;
     chain<TransformedType>(callbackChain: CallbackChain<InitialType, TransformedType>): Maybe<TransformedType>;
     orDefault<DefaultType = InitialType>(defaultValue: DefaultType): DefaultType;
     orDefault(defaultValue: InitialType): InitialType;
+    orDefaultLazy(callbackDefaultLazy: CallbackDefaultLazy<InitialType>): InitialType;
     getSafe(): GetSafeReturn<InitialType>;
+    filter<NextType extends InitialType>(pred: CallbackPredicate<InitialType, NextType>): Maybe<NextType>;
+    filter(pred: (value: InitialType) => boolean): Maybe<InitialType>;
 }
 
 declare class MaybeImp {
