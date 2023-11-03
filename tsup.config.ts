5325575adad8c6c1f5b786ee46ddc728925d1d16
Caique Moraes <ike.moraes93@gmail.com>
2023-11-02 22:17:47 -0300

    build: update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  bundle: true,
   clean: true,
   minify: true,
   dts: true,
